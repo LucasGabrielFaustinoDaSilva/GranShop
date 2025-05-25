23968a6987f7ef392013e3825943fe5ba8912ed4
LucasGabrielFaustinoDaSilva <lucas.gah2000@gmail.com>
2025-05-25 17:19:58 -0300

    Precisa corrigir

--- GranShopAPI/Models/Categoria.cs
+++ GranShopAPI/Models/Categoria.cs
@@ -1,9 +1,16 @@
-// Models/Categoria.cs
-namespace GranShopAPI.Models;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
-public class Categoria
+namespace GranShopAPI.Models
 {
-    public int Id { get; set; }
-    public string Nome { get; set; }
-    public List<Produto> Produtos { get; set; }
+    [Table("Categoria")]
+    public class Categoria
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(30)]
+        public string Nome { get; set; }
+    }
 }
--- GranShopAPI/Models/Produto.cs
+++ GranShopAPI/Models/Produto.cs
@@ -1,16 +1,36 @@
-// Models/Produto.cs
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace GranShopAPI.Models;
 
+[Table("Produto")]
 public class Produto
 {
+    [Key]
     public int Id { get; set; }
+
+    [Required]
+    [StringLength(30)]
     public string Nome { get; set; }
+
+    [Required]
+    [StringLength(500)]  
     public string Descricao { get; set; }
+
+    [Required]
+    [Range(0, int.MaxValue)]
+    [StringLength(100)]
     public int Estoque { get; set; }
+
+    [Required]
+    
+    
     public decimal ValorCusto { get; set; }
     public decimal ValorVenda { get; set; }
     public bool Destaque { get; set; }
+
+    [Required]
     public int CategoriaId { get; set; }
-    
+    [ForeignKey("CategoriaId")]
     public Categoria Categoria { get; set; }
 }
